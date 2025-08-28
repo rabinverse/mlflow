aaca95afb930d3800b843f8304d8df5490ec161d
rabin20-04 <poudelrabin2004@gmail.com>
2025-08-28 22:51:36 +0545

    complete two default experiments

--- src/train.py
+++ src/train.py
@@ -4,12 +4,14 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
-from dvclive.live import Live
+import mlflow
 
 
 data = pd.read_csv("./data/raw/diabetes.csv")
 
 # ---------> filter
+
+
 # print(data.head())
 data["gender"] = data["gender"].map({"Female": 0, "Male": 1})
 data.drop(columns="smoking_history", inplace=True)
@@ -26,14 +28,19 @@ x_train, x_test, y_train, y_test = train_test_split(
 )
 
 
-n_estimators = 90
-max_depth = 20
+n_estimators = 100
+max_depth = 30
 
 
 # model
 rf = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth)
 rf.fit(x_train, y_train) 
 y_pred = rf.predict(x_test)
+
+# accuracy=accuracy_score(y_test, y_pred)
+# precision=precision_score(y_test, y_pred)
+# recall_value=recall_score(y_test, y_pred)
+# f1_score=f1_score(y_test, y_pred)
 
 # just print
 print("Accuracy : ", accuracy_score(y_test, y_pred))
@@ -42,11 +49,11 @@ print("Recall Score : ", recall_score(y_test, y_pred))
 print("f1_score : ", f1_score(y_test, y_pred))
 
 # -------------> log experiment vars     <------------
-with Live(save_dvc_exp=True) as live:
-    live.log_metric("Accuracy : ", float(accuracy_score(y_test, y_pred)))
-    live.log_metric("Precision : ", float(precision_score(y_test, y_pred)))
-    live.log_metric("Recall Score : ", float(recall_score(y_test, y_pred)))
-    live.log_metric("f1_score : ", float(f1_score(y_test, y_pred)))
+with mlflow.start_run():
+    mlflow.log_metric("Accuracy : ", float(accuracy_score(y_test, y_pred)))
+    mlflow.log_metric("Precision : ", float(precision_score(y_test, y_pred)))
+    mlflow.log_metric("Recall Score : ", float(recall_score(y_test, y_pred)))
+    mlflow.log_metric("f1_score : ", float(f1_score(y_test, y_pred)))
 
-    live.log_param("n_estimator", n_estimators)
-    live.log_param("max_depth", max_depth)
+    mlflow.log_param("n_estimator", n_estimators)
+    mlflow.log_param("max_depth", max_depth)
