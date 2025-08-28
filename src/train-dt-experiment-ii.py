021ce037916be2812b3a7f83be51b30729f19374
rabin20-04 <poudelrabin2004@gmail.com>
2025-08-28 23:11:11 +0545

    manual experiment done

--- src/train-dt-experiment-ii.py
+++ src/train-dt-experiment-ii.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 import mlflow
 
@@ -28,12 +28,11 @@ x_train, x_test, y_train, y_test = train_test_split(
 )
 
 
-n_estimators = 100
-max_depth = 30
+max_depth = 50
 
 
 # model
-rf = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth)
+rf = DecisionTreeClassifier(max_depth=max_depth)
 rf.fit(x_train, y_train)
 y_pred = rf.predict(x_test)
 
@@ -49,8 +48,15 @@ print("Recall Score : ", recall_score(y_test, y_pred))
 print("f1_score : ", f1_score(y_test, y_pred))
 
 # -------------> log experiment vars     <------------
+# with mlflow.start_run(): without setexperimet or experiment id they are  stored in the default experiment runs
 
-#track runs in default experiment 
+# mlflow.set_experiment(
+#     "dt-Diabetes_classification"
+# )  # - (new experiment name) if present it creates runs in the experiment if experiment doesnt exists  it will creat one
+# or  with mlflow.start_run(experiment_id=the experiment id):
+# deckare run name to avoid default random names run_name="name"
+
+mlflow.set_experiment("dt-Diabetes_classification")
 
 with mlflow.start_run():
     mlflow.log_metric("Accuracy : ", float(accuracy_score(y_test, y_pred)))
@@ -58,5 +64,4 @@ with mlflow.start_run():
     mlflow.log_metric("Recall Score : ", float(recall_score(y_test, y_pred)))
     mlflow.log_metric("f1_score : ", float(f1_score(y_test, y_pred)))
 
-    mlflow.log_param("n_estimator", n_estimators)
     mlflow.log_param("max_depth", max_depth)
